f4711e7d845c565aff894bfc3339716c625fd939
Ranjib Dey <ranjib@pagerduty.com>
2013-08-02 01:34:49 -0700

    neolithic locks

--- lib/etcd/lock.rb
+++ lib/etcd/lock.rb
@@ -6,26 +6,36 @@ module Etcd
     class LockAcqusitionFailure < StandardError; end
     class LockReleaseFailure < StandardError; end
 
-    attr_reader :lock_id, :key, :value
+    attr_reader :lock_id, :key, :value, :client
+    attr_reader :retries, :retry_interval, :attempts
+
 
     def initialize(opts={})
       @client = opts[:client]
       @key = opts[:key] || '/global/lock' 
       @value = opts[:value] || 0
+      @retries = opts[:retries] || 10
+      @retry_interval = opts[:retry_interval] || 1
+      @attempts = 0
     end
 
     def acquire
       @lock_id =  uuid.generate
       begin
-        client.test_and_set(key, lock_id, value)
+        response = client.test_and_set(key, lock_id, value)
+        @attempts = 0
+        response
       rescue Exception => e  
-        raise LockAcqusitionFailure, e.message
+        @attempts += 1
+        raise LockAcqusitionFailure, e.message if attempts >= retries
+        sleep retry_interval
+        acquire
       end
     end
 
     def release
       begin
-        client.test_and_set(key, value, lock_id)
+        response = client.test_and_set(key, value, lock_id)
       rescue Exception => e
         raise LockReleaseFailure, e.message
       end
