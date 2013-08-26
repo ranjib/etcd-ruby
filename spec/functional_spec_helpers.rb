50e92f238d357a19ba220b4bab924ee23a7bd4b9
Ranjib Dey <ranjib@pagerduty.com>
2013-08-26 01:20:17 -0700

    refactor spec

--- spec/functional_spec_helpers.rb
+++ spec/functional_spec_helpers.rb
@@ -27,10 +27,11 @@ module Etcd
       end
 
       def spawn_etcd_server(dir, client_port=4001, server_port=7001, leader = nil)
+        args = " -c 127.0.0.1:#{client_port} -s 127.0.0.1:#{server_port} -d #{dir} -n node_#{client_port}"
         command = if leader.nil?
-                    ETCD_BIN + " -c 127.0.0.1:#{client_port} -s 127.0.0.1:#{server_port} -d #{dir}"
+                    ETCD_BIN + args
                   else
-                    ETCD_BIN + " -c 127.0.0.1:#{client_port} -s 127.0.0.1:#{server_port} -C #{leader} -d #{dir}"
+                    ETCD_BIN + args + " -C #{leader}"
                   end
         puts command
         pid = spawn(command)
