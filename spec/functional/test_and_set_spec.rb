e9e03d0e9d8425bf16b93570a7c342c2b52523ad
Ranjib Dey <ranjib@pagerduty.com>
2013-12-10 14:49:46 -0800

    support etcd v2 api

--- spec/functional/test_and_set_spec.rb
+++ spec/functional/test_and_set_spec.rb
@@ -14,5 +14,31 @@ shared_examples "test_and_set" do
     client.set(key, value)
     expect{ client.test_and_set(key, 10, 2)}.to raise_error(Net::HTTPServerException)
   end
+
+  it "#create should succeed when the key is absent and update should fail" do
+    key = random_key(2)
+    value = uuid.generate
+    expect {
+      client.update(key, value)
+    }.to raise_error
+    expect {
+      client.create(key, value)
+    }.to_not raise_error
+    expect(client.get(key).value).to eq(value)
+  end
+
+  it "#create should fail when the key is present and update should succeed" do
+    key = random_key(2)
+    value = uuid.generate
+    client.set(key, 1)
+
+    expect {
+      client.create(key, value)
+    }.to raise_error
+
+    expect {
+      client.update(key, value)
+    }.to_not raise_error
+  end
 end
 
